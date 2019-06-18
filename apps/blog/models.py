bcc44af9d283258a9f52fc054713dc0a68945fa9
Dy0mar <kurtudins@gmail.com>
2019-06-18 15:02:28 +0300

    rename user to author

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -11,12 +11,12 @@ class Post(models.Model):
     text = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(
+    author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='post_set'
     )
 
     def __str__(self):
-        return '<Post: {}... User: {}>'.format(self.title[:10], self.user)
+        return '<Post: {}... User: {}>'.format(self.title[:10], self.author)
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
