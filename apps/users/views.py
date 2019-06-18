7890e6d54b1e1ef35133b6554bf5a137d08db0ba
Dy0mar <kurtudins@gmail.com>
2019-06-18 20:39:34 +0300

    Add MyFollowUpList, FeedsListView and subscribe_to

---
+++ apps/users/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView
+
+from blog.models import Post
+from users.models import FollowUp, User
+
+
+class FeedsListView(LoginRequiredMixin, ListView):
+    model = Post
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'posts/post_list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(
+            author__author_set__follower=self.request.user
+        ).order_by('-created_at')
+
+
+feeds = FeedsListView.as_view()
+
+
+class MyFollowUpList(LoginRequiredMixin, ListView):
+    model = FollowUp
+    paginate_by = 10
+    context_object_name = 'subscribe_list'
+    template_name = 'users/subscribe_list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(
+            follower=self.request.user
+        )
+
+
+my_follow_up_list = MyFollowUpList.as_view()
+
+
+def subscribe_to(request, pk):
+    author = get_object_or_404(User, pk=pk)
+    FollowUp.objects.get_or_create(
+        follower=request.user,
+        author=author
+    )
+    return redirect('my-follow-up-list')
